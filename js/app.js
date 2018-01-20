66f6abd5bcd88b0962c898414065b732efb749ac
duke <nolansingroy>
2018-01-19 19:13:46 -0600

    Added nickenames and if else condition for number of clicks to app.js revised app.js to display baby,toddler, and child depending on the number of clicks, made changes to reflect the app.js code within index.html

--- js/app.js
+++ js/app.js
@@ -1,30 +1,38 @@
 // first make view model
 
 
-var ViewModel = function() {
 
-//put model in view model 
+//put model in view model
 //functionality first
 
 
-//create variables 
+//create variables
 //name clickcount img scr imgattribution
-
-
+var Cat = function() {
 this.clickCount = ko.observable(0);
 this.name = ko.observable('Tabby');
 this.imgSrc = ko.observable('img/tabby.jpg')
 this.imgAttribution = ko.observable('https://www.flickr.com/photos/big')
-
-this.incrementCounter = function() {
-    this.clickCount(this.clickCount() +1);
-};
+this.nicknames = ko.observableArray(['small cat','medium cat','large cat']);
 
 
-
-
-
+this.title = ko.computed(function(){
+    var title;
+    var clicks = this.clickCount();
+    if (clicks < 10){
+    title = 'baby';
+}   else if (clicks < 15 ){
+    title = 'toddler';
+}   else {
+    title = 'Child';
+}return title;
+},this);
 }
-
-
+//alert('The first element is ' + myObservableArray()[0]);
+var ViewModel = function() {
+        this.currentCat = ko.observable( new Cat() );
+	this.incrementCounter = function() {
+            this.currentCat().clickCount(this.currentCat().clickCount() +1);
+	};
+}
 ko.applyBindings(new ViewModel());
