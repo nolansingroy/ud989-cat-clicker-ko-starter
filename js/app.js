aa3195c0d089eef9bb9ccb87cec3f356f618eaa2
duke <nolansingroy>
2018-01-22 22:44:59 -0600

    more cats functionality working on adding psuedo database objects/ cats to be push into an array of catItem using forEach, not tested yet

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-// first make view model
+// add cats, but could also load this data froma  server
+var initial Cats = [
+    {
+        clickCount : 0,
+        name : 'Tabby',
+        imgSrc : 'img/434164568_.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
+    },
+    {
+        clickCount : 0,
+        name : 'Tiger',
+        imgScr : 'img/41545.jpg',
+        imgAttribution : 'https://wwflickr.com/photos/xshamx',
+        nicknames: ['Tigger']
+    },
+    {
+    name : 'Scaredy',
+    imgSrc : 'img/415.jpq',
+    imgAttribution : 'https://flickr.com/photos/kpjas',
+    nicknames: ['casper']
+    }
+]
 
 
 
@@ -31,13 +52,15 @@ this.title = ko.computed(function(){
 //alert('The first element is ' + myObservableArray()[0]);
 var ViewModel = function() {
 	var self = this;
-	this.currentCat = ko.observable( new Cat({
-        clickCount:0,
-        name: 'Tabby',
-        imgSrc: 'img/...',
-        imgAttribution: 'https://www.flickr.com/photos/bigtallguy/43416456',
-        nicknames: ['Tabtab','T-Bone','Mr. T', 'Tabitha Tab Tabby Catty Cat']
-        }));
+
+        this.catList = ko.observableArray([]);
+
+        initalCats.forEach(function(catItem)){
+            self.catList.push(new Cat(catItem) );
+        });
+
+	this.currentCat = ko.observable( this.catList() [0] );
+
 	this..incrementCounter = function() {
             self.currentCat().clickCount(self.currentCat().clickCount() +1);
 	};
