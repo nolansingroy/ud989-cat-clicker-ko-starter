48a33c49808cea033a8d07dc7f6c093143236a95
duke <nolansingroy>
2018-01-24 13:04:30 -0600

    Iteration 1 complete update: cats now show up in a list and the current cat change when you click on a cat name in the list, app.js successfully working in firefox

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ ud989-cat-clicker-ko-starter
 
 Here the goal is to use knockout.js to rebuild cat clicker
 by using kockout.js framework you write more effecient code using MVVM
-#Udacity Code
 
+using cat array  to allow for multiple Cats
 
 #documentation
 http://knockoutjs.com/documentation/introduction.html
--- js/app.js
+++ js/app.js
@@ -1,26 +1,40 @@
 // add cats, but could also load this data froma  server
-var initial Cats = [
-    {
-        clickCount : 0,
-        name : 'Tabby',
-        imgSrc : 'img/434164568_.jpg',
-        imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
-    },
-    {
-        clickCount : 0,
-        name : 'Tiger',
-        imgScr : 'img/41545.jpg',
-        imgAttribution : 'https://wwflickr.com/photos/xshamx',
-        nicknames: ['Tigger']
-    },
-    {
-    name : 'Scaredy',
-    imgSrc : 'img/415.jpq',
-    imgAttribution : 'https://flickr.com/photos/kpjas',
+var initialCats = [{
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/small.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568'
+  },
+  {
+    clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/tiger.jpg',
+    imgAttribution: 'https://flickr.com/photos/kpjas',
+    nicknames: ['Tigger']
+  },
+  {
+    clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/ready.jpg',
+    imgAttribution: 'https://flickr.com/photos/kpjas',
     nicknames: ['casper']
-    }
-]
+  },
+  {
+    clickCount: 0,
+    name: 'snow',
+    imgSrc: 'img/small.jpg',
+    imgAttribution: 'https://flickr.com/photos/kpjas',
+    nicknames: ['snowbaby']
+  },
+  {
+    clickCount: 0,
+    name: 'ready',
+    imgSrc: 'img/ready.jpg',
+    imgAttribution: 'https://flickr.com/photos/kpjas',
+    nicknames: ['babyCakes']
+  }
 
+]
 
 
 //put model in view model
@@ -29,40 +43,48 @@ var initial Cats = [
 
 //create variables
 //name clickcount img scr imgattribution
-var Cat = function() {
-this.clickCount = ko.observable(data.clickCount);
-this.name = ko.observable(data.name);
-this.imgSrc = ko.observable(data.imgSrc);
-this.imgAttribution = ko.observable(data.imgAttribution);
-this.nicknames = ko.observableArray(data.nicknames);
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
+  this.nicknames = ko.observableArray(data.nicknames);
 
 
-this.title = ko.computed(function(){
+  this.title = ko.computed(function() {
     var title;
     var clicks = this.clickCount();
-    if (clicks < 10){
-    title = 'baby';
-}   else if (clicks < 15 ){
-    title = 'toddler';
-}   else {
-    title = 'Child';
-}return title;
-},this);
+    if (clicks < 10) {
+      title = 'baby';
+    } else if (clicks < 15) {
+      title = 'toddler';
+    } else {
+      title = 'Child';
+    }
+    return title;
+  }, this);
 }
 //alert('The first element is ' + myObservableArray()[0]);
+
 var ViewModel = function() {
-	var self = this;
+  var self = this;
 
-        this.catList = ko.observableArray([]);
+  this.catList = ko.observableArray([]);
 
-        initalCats.forEach(function(catItem)){
-            self.catList.push(new Cat(catItem) );
-        });
+  initialCats.forEach(function(catItem) {
+    self.catList.push(new Cat(catItem));
+  });
 
-	this.currentCat = ko.observable( this.catList() [0] );
+this.currentCat = ko.observable(this.catList()[0]);
 
-	this..incrementCounter = function() {
-            self.currentCat().clickCount(self.currentCat().clickCount() +1);
-	};
-}
+this.incrementCounter = function() {
+  self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+};
+
+this.setCat = function(clickedCat) {
+  console.log('hi');
+    self.currentCat(clickedCat);
+};
+
+};
 ko.applyBindings(new ViewModel());
